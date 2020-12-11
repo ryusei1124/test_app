bb3ffebe031b3e721333755096ee600fc5c0e800
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2020-12-11 04:10:00 +0000

    8.2~8.4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,4 +31,18 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+    
+  
 end
